bc5264c517b83b76dfcba16b9017de6271a49535
Flavio <fflaviojr@outlook.com>
2019-02-07 14:41:20 -0200

    new

--- js/custom.js
+++ js/custom.js
@@ -1,363 +1,119 @@
-/* JS Document */
 
-/******************************
+$(document).ready(function() {
 
-[Table of Contents]
+  // Smooth scrolling
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-1. Vars and Inits
-2. Set Header
-3. Init Menu
-4. Init Main Slider
-5. Init Progress Bars
-6. Init Testimonials Slider
-7. Init Clients Slider
-8. Init Parallax
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, 'easeInOutExpo');
 
+          if ( $(this).parents('.nav-menu').length ) {
+            $('.nav-menu .menu-active').removeClass('menu-active');
+            $(this).closest('li').addClass('menu-active');
+          }
 
-******************************/
+          if ( $('body').hasClass('mobile-nav-active') ) {
+              $('body').removeClass('mobile-nav-active');
+              $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+              $('#mobile-body-overly').fadeOut();
+          }
+          return false;
+        }
+      }
+    });
+  });
 
-$(document).ready(function()
-{
-	"use strict";
+  // Initiate superfish on nav menu
+  $('.nav-menu').superfish({
+    animation: {opacity:'show'},
+    speed: 400
+  });
 
-	/* 
+  // Mobile Navigation
+  if( $('#nav-menu-container').length ) {
+      var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
+      $mobile_nav.find('> ul').attr({ 'class' : '', 'id' : '' });
+      $('body').append( $mobile_nav );
+      $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
+      $('body').append( '<div id="mobile-body-overly"></div>' );
+      $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
-	1. Vars and Inits
+      $(document).on('click', '.menu-has-children i', function(e){
+          $(this).next().toggleClass('menu-item-active');
+          $(this).nextAll('ul').eq(0).slideToggle();
+          $(this).toggleClass("fa-chevron-up fa-chevron-down");
+      });
 
-	*/
+      $(document).on('click', '#mobile-nav-toggle', function(e){
+          $('body').toggleClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').toggle();
+      });
 
-	var ctrl = new ScrollMagic.Controller();
-	var controller = new ScrollMagic.Controller();
-	var header = $('.header');
-	var menuActive = false;
-	var hamb = $('.hamburger_container');
-	var menu = $('.fs_menu_container');
-	var hambIcon = $('.hamburger_icon');
+      $(document).click(function (e) {
+          var container = $("#mobile-nav, #mobile-nav-toggle");
+          if (!container.is(e.target) && container.has(e.target).length === 0) {
+             if ( $('body').hasClass('mobile-nav-active') ) {
+                  $('body').removeClass('mobile-nav-active');
+                  $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                  $('#mobile-body-overly').fadeOut();
+              }
+          }
+      });
+  } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
+      $("#mobile-nav, #mobile-nav-toggle").hide();
+  }
 
-	setHeader();
+  // Stick the header at top on scroll
+  $("#header").sticky({topSpacing:0, zIndex: '50'});
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+  // Counting numbers
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
 
-	initMenu();
-	initMainSlider();
-	initProgressBars();
-	initTestimonialsSlider();
-	initClientsSlider();
-	initParallax();
+  // Tooltip & popovers
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
 
-	/* 
+  // Background image via data tag
+  $('[data-block-bg-img]').each(function() {
+    // @todo - invoke backstretch plugin if multiple images
+    var $this = $(this),
+      bgImg = $this.data('block-bg-img');
 
-	2. Set Header
+      $this.css('backgroundImage','url('+ bgImg + ')').addClass('block-bg-img');
+  });
 
-	*/
+  // jQuery counterUp
+  if(jQuery().counterUp) {
+    $('[data-counter-up]').counterUp({
+      delay: 20,
+    });
+  }
 
-	function setHeader()
-	{
-		if(window.innerWidth < 992)
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.css({'height':"80"});
-			}
-			else
-			{
-				header.css({'height':"110"});
-			}
-		}
-		else
-		{
-			if($(window).scrollTop() > 100)
-			{
-				header.css({'height':"80"});
-			}
-			else
-			{
-				header.css({'height':"110"});
-			}
-		}
-		if(window.innerWidth > 991 && menuActive)
-		{
-			closeMenu();
-		}
-	}
+  //Scroll Top link
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.scrolltop').fadeIn();
+    } else {
+      $('.scrolltop').fadeOut();
+    }
+  });
 
-	/* 
+  $('.scrolltop, #logo a').click(function(){
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000, 'easeInOutExpo');
+    return false;
+  });
 
-	3. Init Menu
-
-	*/
-
-	function initMenu()
-	{
-		if($('.hamburger_container').length)
-		{
-			hamb.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
-		}
-	}
-
-	function openMenu()
-	{
-		menu.addClass('active');
-		setTimeout(function()
-		{
-			hambIcon.addClass('active');
-		},500);
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		setTimeout(function()
-		{
-			hambIcon.removeClass('active');
-		},500);
-		menuActive = false;
-	}
-
-	/* 
-
-	4. Init Main Slider
-
-	*/
-
-	function initMainSlider()
-	{
-		if($('.hero_slider').length)
-		{
-			var heroSlider = $('.hero_slider');
-
-			heroSlider.owlCarousel({
-				items: 1,
-				loop: true,
-				autoplay:true,
-				autoplayTimeout:5000,
-				autoplaySpeed:800,
-				smartSpeed:800,
-				dotsContainer:'main_slider_custom_dots'
-			});
-
-			/* Custom dots events */
-			if($('.main_slider_custom_dot').length)
-			{
-				$('.main_slider_custom_dot').on('click', function()
-				{
-					$('.main_slider_custom_dot').removeClass('active');
-					$(this).addClass('active');
-					heroSlider.trigger('to.owl.carousel', [$(this).index(), 300]);
-				});
-			}
-
-			/* Change active class for dots when slide changes by nav or touch */
-			heroSlider.on('changed.owl.carousel', function(event)
-			{
-				$('.main_slider_custom_dot').removeClass('active');
-				$('.main_slider_custom_dots li').eq(event.page.index).addClass('active');
-			});	
-
-			/* Custom navigation */
-			if($('.main_slider_nav').length)
-			{
-				$('.main_slider_nav_left').on('click', function()
-				{
-					heroSlider.trigger('prev.owl.carousel');
-				});
-
-				$('.main_slider_nav_right').on('click', function()
-				{
-					heroSlider.trigger('next.owl.carousel');
-				});
-			}
-
-		}
-	}
-
-	/* 
-
-	5. Init Progress Bars
-
-	*/
-
-	function initProgressBars()
-	{
-		if($('.skill_bars').length)
-		{
-			var bars = $('.skill_bars');
-
-			bars.each(function()
-			{
-				var ele = $(this);
-	    		var elePerc = ele.data('perc');
-	    		var eleName = '#'+ele.data('name');
-
-	    		var statsScene = new ScrollMagic.Scene({
-		    		triggerElement: this,
-		    		triggerHook: 'onEnter',
-		    		reverse:false
-		    	})
-		    	.on('start', function()
-		    	{
-		    		var pbar = new ProgressBar.Line(eleName, 
-		    		{
-		    			strokeWidth: 0.5,
-						easing: 'easeInOut',
-						duration: 1400,
-						color: '#ff4200',
-						trailColor: 'rgba(255,255,255,1)',
-						trailWidth: 1,
-						svgStyle: {display: 'block', width: '100%', height: '100%'},
-						text: {
-							style: {
-								// Text color.
-								// Default: same as stroke color (options.color)
-								color: '#FFFFFF',
-								position: 'absolute',
-								left: 'calc' + '(' + (elePerc * 100) + '% - 48px' + ')',
-								top: '17px',
-								padding: 0,
-								margin: 0,
-								transform: null
-								},
-								autoStyleContainer: false
-						},
-						from: {color: '#00bcd5'},
-						to: {color: '#00bcd5'},
-						step: function(state, bar) {
-						bar.setText(Math.round(bar.value() * 100) + ' %');
-						}
-		    		});
-		    		pbar.animate(elePerc);
-		    	})
-		    	.addTo(ctrl);
-			})
-		}
-	}
-
-	/* 
-
-	6. Init Testimonials Slider
-
-	*/
-
-	function initTestimonialsSlider()
-	{
-		if($('.testimonials_slider').length)
-		{
-			var testSlider = $('.testimonials_slider');
-
-			testSlider.owlCarousel(
-			{
-				items:1,
-				loop:true,
-				dots:false
-			});
-
-			if($('.test_slider_nav').length)
-			{
-				var navLeft = $('.test_slider_nav_left');
-				var navRight = $('.test_slider_nav_right');
-
-				navLeft.on('click', function()
-				{
-					testSlider.trigger('prev.owl.carousel');
-				});
-
-				navRight.on('click', function()
-				{
-					testSlider.trigger('next.owl.carousel');
-				});
-			}
-		}
-	}
-
-	/* 
-
-	7. Init Clients Slider
-
-	*/
-
-	function initClientsSlider()
-	{
-		if($('.clients_slider').length)
-		{
-			var clientsSlider = $('.clients_slider');
-
-			clientsSlider.owlCarousel(
-			{
-				loop: true,
-				dots: false,
-				responsive:
-				{
-					0:{items:1},
-					575:{items:2},
-					768:{items:3},
-					992:{items:4},
-					1199:{items:5}
-					
-				}
-			});
-		}
-	}
-
-	/* 
-
-	8. Init Parallax
-
-	*/
-
-	function initParallax()
-	{
-		// Add parallax effect to home slider
-		if($('.slider_prlx').length)
-		{
-			var homeBcg = $('.slider_prlx');
-
-			var homeBcgScene = new ScrollMagic.Scene({
-		        triggerElement: homeBcg,
-		        triggerHook: 1,
-		        duration: "100%"
-		    })
-		    .setTween(TweenMax.to(homeBcg, 1, {y: '15%', ease:Power0.easeNone}))
-		    .addTo(ctrl);
-		}
-
-		// Add parallax effect to every element with class prlx
-		// Add class prlx_parent to the parent of the element
-		if($('.prlx_parent').length && $('.prlx').length)
-		{
-			var elements = $('.prlx_parent');
-
-			elements.each(function()
-			{
-				var ele = this;
-				var bcg = $(ele).find('.prlx');
-
-				var slideParallaxScene = new ScrollMagic.Scene({
-			        triggerElement: ele,
-			        triggerHook: 1,
-			        duration: "200%"
-			    })
-			    .setTween(TweenMax.from(bcg, 1, {y: '-30%', ease:Power0.easeNone}))
-			    .addTo(ctrl);
-			});
-		}
-	}
 });
